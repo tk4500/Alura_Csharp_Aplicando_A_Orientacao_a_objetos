379b39f9d0066df1df0b9e6c27ac2de5ce584003
tk4500 <131721143+tk4500@users.noreply.github.com>
2024-06-05 17:35:17 -0300

    Criando a lista de músicas

--- ScreenSound/ScreenSound/Album.cs
+++ ScreenSound/ScreenSound/Album.cs
@@ -1,11 +1,12 @@
 ﻿class Album
 {
+    private List<Musica> musicas = new List<Musica>();
     public string Nome { get; set; }
 
     public int DuracaoTotal { get; set; }
 
     public void AdicionarMusica(Musica musica)
     {
-
+        musicas.Add(musica);
     }
 }
