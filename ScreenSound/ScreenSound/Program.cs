aff7e47ede9988039c601b5022bc11da2b9a2c4d
tk4500 <131721143+tk4500@users.noreply.github.com>
2024-06-05 09:52:43 -0300

    Objetos

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();
+
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+
+Console.WriteLine($"Música: {musica1.nome}");
+Console.WriteLine($"Artista: {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+
+Console.WriteLine($"Música: {musica2.nome}");
+Console.WriteLine($"Artista: {musica2.artista}");
