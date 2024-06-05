f0bff50b997bf8ab43f45f0c86b19aaaf0525c6b
tk4500 <131721143+tk4500@users.noreply.github.com>
2024-06-05 19:49:59 -0300

    Podcast

---
+++ ScreenSound/ScreenSound/Podcast.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+class Podcast
+{
+    public Podcast(string nome, string host)
+    {
+        Nome = nome;
+        Host = host;
+    }
+
+    private List<Episodio> episodios = new();
+    public string Nome { get; }
+
+
+    public string Host { get; }
+
+    public int TotalEpisodios => episodios.Count;
+
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Podcast {Nome} apresentado por {Host}\n");
+        foreach (Episodio episodio in episodios.OrderBy(e => e.Ordem))
+        {
+            Console.WriteLine(episodio.Resumo);
+        }
+        Console.WriteLine($"Este podcast possui {TotalEpisodios} episódios.");
+    }
+
+}
--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,4 +1,13 @@
-﻿Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+﻿Episodio ep1 = new(2, "Técnicas de facilitação", 45);
 ep1.AdicionarConvidados("Maria");
 ep1.AdicionarConvidados("Marcelo");
-Console.WriteLine(ep1.Resumo);
+
+Episodio ep2 = new(1, "Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flavia");
+
+Podcast podcast = new("Podcast Especial", "Daniel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
