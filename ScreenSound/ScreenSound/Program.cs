316b04dac720c41f5c1afa9f5dfeb3d255c1d744
tk4500 <131721143+tk4500@users.noreply.github.com>
2024-06-05 19:36:59 -0300

    Episodio

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,25 +1,4 @@
-﻿Banda queen = new Banda("Queen");
-
-Album albumDoQueen = new Album("A night at the opera");
-
-Musica musica1 = new Musica(queen, "Love of my life")
-{
-    Duracao = 213,
-    Disponivel = true,
-};
-
-
-Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
-{
-    Duracao = 354,
-    Disponivel = false,
-};
-
-albumDoQueen.AdicionarMusica(musica1);
-albumDoQueen.AdicionarMusica(musica2);
-queen.AdicionarAlbum(albumDoQueen);
-
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
-albumDoQueen.ExibirMusicasDoAlbum();
-queen.ExibirDiscografia();
+﻿Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
+Console.WriteLine(ep1.Resumo);
